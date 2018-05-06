4cf88ce512e2760da3bcf40e058388635862ccea
Raph <rafal83@users.noreply.github.com>
2018-05-06 08:26:05 +0200

    Update reset.sh
    ADB ne s'arrête pas toujours et donc la connexion ne fonctionne pas après !

--- 3rdparty/reset.sh
+++ 3rdparty/reset.sh
@@ -6,6 +6,8 @@ echo "########### Reset ##########"
 echo Arrêt des services en cours :
 echo `$sudo_prefix adb kill-server`
 
+echo `$sudo_prefix killall adb`
+
 echo Redémarrage des services :
 echo `$sudo_prefix adb start-server`
 echo "########### Fin ##########"
